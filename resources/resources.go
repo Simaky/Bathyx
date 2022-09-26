895284e781472c860dad56d9087ed52b9cb5c481
Simaky <maxympritika@gmail.com>
2022-09-26 19:37:49 +0300

    Code refactoring, add unit test, add retries logic

--- resources/resources.go
+++ resources/resources.go
@@ -1,6 +1,9 @@
 package resources
 
-import _ "embed"
+import (
+	_ "embed"
+	"fmt"
+)
 
 var (
 	//go:embed images/logo.ico
@@ -13,32 +16,69 @@ var (
 	HeadphonesNotConnectedIcon []byte
 
 	//go:embed images/battery/10.ico
-	BatteryIcon10 []byte
+	batteryIcon10 []byte
 
 	//go:embed images/battery/20.ico
-	BatteryIcon20 []byte
+	batteryIcon20 []byte
 
 	//go:embed images/battery/30.ico
-	BatteryIcon30 []byte
+	batteryIcon30 []byte
 
 	//go:embed images/battery/40.ico
-	BatteryIcon40 []byte
+	batteryIcon40 []byte
 
 	//go:embed images/battery/50.ico
-	BatteryIcon50 []byte
+	batteryIcon50 []byte
 
 	//go:embed images/battery/60.ico
-	BatteryIcon60 []byte
+	batteryIcon60 []byte
 
 	//go:embed images/battery/70.ico
-	BatteryIcon70 []byte
+	batteryIcon70 []byte
 
 	//go:embed images/battery/80.ico
-	BatteryIcon80 []byte
+	batteryIcon80 []byte
 
 	//go:embed images/battery/90.ico
-	BatteryIcon90 []byte
+	batteryIcon90 []byte
 
 	//go:embed images/battery/100.ico
-	BatteryIcon100 []byte
+	batteryIcon100 []byte
+
+	batteries = map[int][]byte{
+		10:  batteryIcon10,
+		20:  batteryIcon20,
+		30:  batteryIcon30,
+		40:  batteryIcon40,
+		50:  batteryIcon50,
+		60:  batteryIcon60,
+		70:  batteryIcon70,
+		80:  batteryIcon80,
+		90:  batteryIcon90,
+		100: batteryIcon100,
+	}
 )
+
+// GetBatteryIcon returns battery icon for given percentage (10-100).
+func GetBatteryIcon(percentage int) ([]byte, error) {
+	batteryIcon, ok := batteries[roundPercentage(percentage)]
+	if !ok {
+		return nil, fmt.Errorf("can't get battery icon, got percentage: %d", percentage)
+	}
+	return batteryIcon, nil
+}
+
+func roundPercentage(percentage int) int {
+	if percentage <= 0 {
+		return 10
+	}
+	if percentage > 100 {
+		return 100
+	}
+
+	if percentage%10 == 0 {
+		return percentage
+	}
+
+	return (10 - percentage%10) + percentage
+}
